93c96d7b124b12511dc6da9d4c99c1f45f089d56
Diptorr <78503363+Diptorr@users.noreply.github.com>
2023-05-13 23:12:55 +0200

    Update mod.rs

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -6,6 +6,12 @@ mod todo;
 
 #[derive(ThisError, Debug)]
 pub enum Error {
+    #[error("Entity Not Found - {0}[{1}]")]
+    EntityNotFound(&'static str, String),
+
     #[error(transparent)]
     SqlxError(#[from] sqlx::Error),
+
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
 }
